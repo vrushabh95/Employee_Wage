ef9cf34d830a87481c3e5cedc1025169c30e2103
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 15:57:41 +0530

    Calculating Wagesa condition of total

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,26 +2,36 @@
 
  echo "Welcome"
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=20;
+# CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+# VARIABLES
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	(( totalWorkingDays++ ))
 	empCheck=$(( RANDOM % 3 ));
 
 	case $empCheck in
-		$isFullTime)
-			empHrs=8;;
-		$isPartTime)
-			empHrs=4;;
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
 		*)
-			empHrs=0;;
+			empHrs=0
+			;;
 	esac
 
-	salary=$(( $empHrs * $empRatePerHr ));
-	totalSalary=$(( $totalSalary + $salary ));
+	totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
 
 done
+
+totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR ));
