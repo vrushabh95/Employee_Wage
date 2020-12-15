2ff9b4cc0b4bff361201b5caf398e0511bf02e76
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 15:24:27 +0530

    Adding Part Time Employee And Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,17 +2,21 @@
 
  echo "Welcome"
 
-isPresent=1;
-randomCheck=$(( RANDOM % 2 ));
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$(( RANDOM % 3 ));
 
-if [ $isPresent -eq $randomCheck ];
+if [ $isFullTime -eq $randomCheck ];
 then
-        empRatePerHr=20;
-        empHrs=8;
-        salary=$(( $empHrs * $empRatePerHr ));
-        echo "Salary of an Employee is $salary"
+	empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+	empHrs=4;
 else
-        salary=0;
-        echo "Employee is Absent"
+	empHrs=0;
 fi
 
+salary=$(( $empHrs * $empRatePerHr ));
+
+echo "Salary of an Employee is $salary"
