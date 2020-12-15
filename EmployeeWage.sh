873a396a0538848ca9f281cf34686df06500f40d
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 15:01:12 +0530

    Checking Employee Attendence UC1

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
+
  echo "Welcome"
+
+
+isPresent=1;
+randomCheck=$(( RANDOM % 2 ));
+
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is Present";
+else
+        echo "Employee is Absent";
+fi
+
 
