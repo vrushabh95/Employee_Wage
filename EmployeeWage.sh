139a64d25f63be2a37c6e9261e276456a13397df
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 15:47:51 +0530

    Calculating Wages For A Month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,16 +4,24 @@
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-empCheck=$(( RANDOM % 3 ));
+numWorkingDays=20;
 
-case $empCheck in
-	$isFullTime)
-		empHrs=8;;
-	$isPartTime)
-		empHrs=4;;
-	*)
-		empHrs=0;;
-esac
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$(( RANDOM % 3 ));
 
-salary=$(( $empHrs * $empRatePerHr ));
+	case $empCheck in
+		$isFullTime)
+			empHrs=8;;
+		$isPartTime)
+			empHrs=4;;
+		*)
+			empHrs=0;;
+	esac
+
+	salary=$(( $empHrs * $empRatePerHr ));
+	totalSalary=$(( $totalSalary + $salary ));
+
+done
