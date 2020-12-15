3ec7442828befbb976ec1d8c2a7031d5bd9f5a87
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 15:15:08 +0530

    Calculating Employee Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,15 +2,17 @@
 
  echo "Welcome"
 
-
 isPresent=1;
 randomCheck=$(( RANDOM % 2 ));
 
 if [ $isPresent -eq $randomCheck ];
 then
-        echo "Employee is Present";
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(( $empHrs * $empRatePerHr ));
+        echo "Salary of an Employee is $salary"
 else
-        echo "Employee is Absent";
+        salary=0;
+        echo "Employee is Absent"
 fi
-
 
