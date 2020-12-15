2d58592bd695206c40f2e25cf660f8e11fce6271
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-15 16:25:07 +0530

    Dictionary:Storing Day And Daily Wage Along With Total Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -5,7 +5,7 @@
 # CONSTANTS FOR THE PROGRAM
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
-MAX_HRS_IN_MONTH=10;
+MAX_HRS_IN_MONTH=4;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
@@ -39,8 +39,9 @@ do
 	(( totalWorkingDays++ ))
 	workHours="$( getWorkingHours $(( RANDOM % 3 )) )"
 	totalWorkHours=$(( $totalWorkHours + $workHours ))
-	empDailyWage[ $totalWorkingDays ]="$( calcDailyWage $workHours )"
+	empDailyWage["$totalWorkingDays"]="$( calcDailyWage $workHours )"
 done
 
 totalSalary="$( calcDailyWage $totalWorkHours )"
 echo "Daily Wage " ${empDailyWage[@]}
+echo "All Keys " ${!empDailyWage[@]}
